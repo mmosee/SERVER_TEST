98e4b6e112d595486a946bc559ef2e9da7efcae3
mmosee <nammosee@gmail.com>
2024-05-03 15:33:17 +0900

    POST 추가

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ app.get('/input', function(req, res) {
     return res.status(200).json({});
 });
 
+// POST TEST
+app.post('/input', function(req, res) {
+    console.log('TEST POST - input');
+
+    console.log(' req.body', JSON.stringify(req.body));
+    
+    return res.status(200).json({});
+});
+
 // 서버를 시작합니다.
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
