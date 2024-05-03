fa57784f11f166cb2d13969e4f3b82d4661fab04
mmosee <nammosee@gmail.com>
2024-05-03 15:22:58 +0900

    f c

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 8800;
+
+// index.html 파일을 제공합니다.
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// GET TEST
+app.get('/input', function(req, res) {
+    console.log('TEST GET - input');
+    
+    return res.status(200).json({});
+});
+
+// 서버를 시작합니다.
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
